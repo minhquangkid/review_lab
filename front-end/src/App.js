bab04e8fae78c4c25c99bffd9215357d607b0c66
quang <minhquangkid@gmail.com>
2024-06-30 16:02:48 +0700

    init done

---
+++ front-end/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    // first
+    // return () => {
+    //   second
+    // }
+  }, []);
+
+  return <React.Fragment></React.Fragment>;
+}
+
+export default App;
